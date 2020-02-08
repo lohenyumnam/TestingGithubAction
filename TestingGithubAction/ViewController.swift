0b3361028b7cc1f05ade8cdff4cb0abc8a0f12a9
LohenYumnam <lohensana@gmail.com>
2020-02-08 09:36:21 +0530

    Test changes

--- TestingGithubAction/ViewController.swift
+++ TestingGithubAction/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello world Github Action iOS")
+        print("Hello world Github Action iOS.")
     }
 
 
